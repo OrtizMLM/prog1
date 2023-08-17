194c9799b9505e1001b5c0ef2e72cde6260798be
Mauro <mauro.ortiz@alumnos.frm.utn.edu.ar>
2023-08-17 10:39:17 -0300

    Prueba

---
+++ app.py
@@ -0,0 +1,26 @@
+import requests, re
+from bs4 import BeautifulSoup as b 
+
+website = "https://dolarhoy.com"
+resultado =  requests.get(website)
+content = resultado.content
+soup = b(content,"html")
+
+linea = soup.find("div", {"class":"venta"},)
+casteo = str(linea)
+quitar = '<div class="venta"><div class="label">Venta</div><div class="val">'
+
+regex = re.compile(quitar)
+
+prueba = regex.sub('', casteo)
+
+quit = '</div></div>'
+regex1 = re.compile(quit)
+pruebaf = regex1.sub('', prueba)
+
+
+sincaste = pruebaf.replace("$","")
+valor = int(sincaste)
+print(valor)
+
+
