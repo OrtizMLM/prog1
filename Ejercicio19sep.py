59123c90061cbf3e8134c289ae30ce0ce585996e
mauro.ortiz@alumnos.frm.utn.edu.ar <mauro.ortiz@alumnos.frm.utn.edu.ar>
2023-09-19 12:11:40 -0300

    Completo ejercicio

--- Ejercicio19sep.py
+++ Ejercicio19sep.py
@@ -4,17 +4,20 @@ def add (dig):
     sum = 0
     for i in dig:    
         sum += int(i)
-    print(sum)
+    print(f"Y la suma de sus digitos es: {sum}")
     return sum
 
 #Main
 
 num = str(input("Ingrese un numero: "))
+acum = int(num)
 while int(num) != 0:
     suma = add(num)
     num = str(input("Ingrese un numero: "))
-    
-    
+    acum += int(num)
+    print(f"La suma total de los numeros ingresados es: {acum}")
+    if int(num) == 0:
+        suma = add(str(acum))
 
 
 
